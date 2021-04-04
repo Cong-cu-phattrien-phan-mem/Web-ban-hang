5942b733e8db06a0374f19ea49304e010515fea8
kiet <trandinhkiet04041999@gmail.com>
2021-04-04 14:23:50 +0700

    add BlogCotroller

---
+++ Controllers/BlogCotroller.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Post1()
+        {
+            return View();
+        }
+        public ActionResult Post2()
+        {
+            return View();
+        }
+        public ActionResult Post3()
+        {
+            return View();
+        }
+    }
+}using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Post1()
+        {
+            return View();
+        }
+        public ActionResult Post2()
+        {
+            return View();
+        }
+        public ActionResult Post3()
+        {
+            return View();
+        }
+    }
+}
